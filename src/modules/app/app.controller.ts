9a8735f7304d77495754d33e9e7d82a773a18400
Pranay Jumde <pranay.jumde@qodequay.com>
2025-04-11 18:23:53 +0530

    added the controller method

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateAppDto, CreateAppDtoType } from './dto/create-app.dto';
 
 @Controller('app')
 export class AppController {
   @Get()
   getHello(): string {
     return 'Hello, World!';
+  }
+
+  @Post()
+  createApp(@Body() body: CreateAppDtoType): string {
+    const parsed = CreateAppDto.parse(body);
+    return `Received : ${JSON.stringify(parsed)}`;
   }
 }
