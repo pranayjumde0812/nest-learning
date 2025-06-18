9e9117af3d3c27cd5eb81c3ad7e454c0bfbe01ef
Pranay Jumde <pranay.jumde@qodequay.com>
2025-06-18 11:52:10 +0530

    upated

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateAppDto, CreateAppDtoType } from './dto/create-app.dto';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('/App')
 @Controller('app')
 export class AppController {
   @Get()
@@ -9,6 +11,16 @@ export class AppController {
   }
 
   @Post()
+  @ApiBody({
+    description: 'Request body for creating an app',
+    schema: {
+      type: 'object',
+      properties: {
+        name: { type: 'string', example: 'John Doe' },
+        age: { type: 'number', example: 25 },
+      },
+    },
+  })
   createApp(@Body() body: CreateAppDtoType): string {
     const parsed = CreateAppDto.parse(body);
     return `Received : ${JSON.stringify(parsed)}`;
